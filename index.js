dff0071c08160bcd4a5d2b30eb523e1081d2b062
Alpysbayev Adilet Kanagatuly <96419013+alpysbayev@users.noreply.github.com>
2023-10-07 11:30:42 +0600

    Update index.js

--- index.js
+++ index.js
@@ -3,14 +3,19 @@ const path = require("path");
 
 const app = express();
 
+// Define the path to your static files (game.html, CSS, JavaScript, etc.)
+const staticPath = path.join(__dirname, 'static');
 
-app.get("/", (req, res) =>{
-    res.sendFile(path.join(__dirname+"/game.html"));
-})
+// Serve the static HTML file
+app.get("/", (req, res) => {
+    res.sendFile(path.join(staticPath, "game.html"));
+});
 
-app.use(express.static('static'));
+// Serve static assets from the 'static' directory
+app.use(express.static(staticPath));
 
+const port = process.env.PORT || 5000; // Use the specified port or 5000 as a default
 
-const server = app.listen(5000);
-const portNumber = server.address().port;
-console.log("port: "+portNumber)
+const server = app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
